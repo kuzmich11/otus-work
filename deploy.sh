bfae0656351b198e075b4590a10f4d81f4d71a25
Vladimir <v.a.kuznetsov-83@yandex.ru>
2024-03-10 19:57:14 +0300

    Test commit

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-sudo cp ./deploy/nginx.conf /etc/nginx/conf.d/demo.conf -f
-sudo sed -i -- "s|%SERVER%|$1|g" /etc/nginx/conf.d/demo.conf
-sudo service nginx restart
-sudo -u www-data composer install -q
-sudo service php8.1-fpm restart
-sudo -u www-data sed -i -- "s|%DATABASE_HOST%|$2|g" .env
-sudo -u www-data sed -i -- "s|%DATABASE_USER%|$3|g" .env
-sudo -u www-data sed -i -- "s|%DATABASE_PASSWORD%|$4|g" .env
-sudo -u www-data sed -i -- "s|%DATABASE_NAME%|$5|g" .env
-sudo -u www-data php bin/console doctrine:migrations:migrate --no-interaction
-sudo -u www-data sed -i -- "s|%RABBITMQ_HOST%|$6|g" .env
-sudo -u www-data sed -i -- "s|%RABBITMQ_USER%|$7|g" .env
-sudo -u www-data sed -i -- "s|%RABBITMQ_PASSWORD%|$8|g" .env
+cp ./deploy/nginx.conf /etc/nginx/conf.d/demo.conf -f
+sed -i -- "s|%SERVER%|$1|g" /etc/nginx/conf.d/demo.conf
+service nginx restart
+-u www-data composer install -q
+service php8.1-fpm restart
+-u www-data sed -i -- "s|%DATABASE_HOST%|$2|g" .env
+-u www-data sed -i -- "s|%DATABASE_USER%|$3|g" .env
+-u www-data sed -i -- "s|%DATABASE_PASSWORD%|$4|g" .env
+-u www-data sed -i -- "s|%DATABASE_NAME%|$5|g" .env
+-u www-data php bin/console doctrine:migrations:migrate --no-interaction
+-u www-data sed -i -- "s|%RABBITMQ_HOST%|$6|g" .env
+-u www-data sed -i -- "s|%RABBITMQ_USER%|$7|g" .env
+-u www-data sed -i -- "s|%RABBITMQ_PASSWORD%|$8|g" .env
