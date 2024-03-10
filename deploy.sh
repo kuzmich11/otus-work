480228c9fc3d5fd5b3a819a794cc04bd57968b60
Vladimir <v.a.kuznetsov-83@yandex.ru>
2024-03-10 20:50:16 +0300

    Test commit

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-sudo cp ./deploy/nginx.conf /etc/nginx/conf.d/demo.conf -f
-sudo sed -i -- "s|%SERVER%|$SERVER|g" /etc/nginx/conf.d/demo.conf
-sudo service nginx restart
-sudo -u www-data composer install -q
-sudo service php8.1-fpm restart
-sudo -u www-data sed -i -- "s|%DATABASE_HOST%|$DATABASE_HOST|g" .env
-sudo -u www-data sed -i -- "s|%DATABASE_USER%|$DATABASE_USER|g" .env
-sudo -u www-data sed -i -- "s|%DATABASE_PASSWORD%|$DATABASE_PASSWORD|g" .env
-sudo -u www-data sed -i -- "s|%DATABASE_NAME%|$DATABASE_NAME|g" .env
-sudo -u www-data php bin/console doctrine:migrations:migrate --no-interaction
-sudo -u www-data sed -i -- "s|%RABBITMQ_HOST%|$RABBITMQ_HOST|g" .env
-sudo -u www-data sed -i -- "s|%RABBITMQ_USER%|$RABBITMQ_USER|g" .env
-sudo -u www-data sed -i -- "s|%RABBITMQ_PASSWORD%|$RABBITMQ_PASSWORD|g" .env
+cp ./deploy/nginx.conf /etc/nginx/conf.d/demo.conf -f
+sed -i -- "s|%SERVER%|$SERVER|g" /etc/nginx/conf.d/demo.conf
+service nginx restart
+composer install -q
+service php8.1-fpm restart
+sed -i -- "s|%DATABASE_HOST%|$DATABASE_HOST|g" .env
+sed -i -- "s|%DATABASE_USER%|$DATABASE_USER|g" .env
+sed -i -- "s|%DATABASE_PASSWORD%|$DATABASE_PASSWORD|g" .env
+sed -i -- "s|%DATABASE_NAME%|$DATABASE_NAME|g" .env
+php bin/console doctrine:migrations:migrate --no-interaction
+sed -i -- "s|%RABBITMQ_HOST%|$RABBITMQ_HOST|g" .env
+sed -i -- "s|%RABBITMQ_USER%|$RABBITMQ_USER|g" .env
+sed -i -- "s|%RABBITMQ_PASSWORD%|$RABBITMQ_PASSWORD|g" .env
