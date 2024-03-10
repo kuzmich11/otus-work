c19cc3699a87361d7d7e0a525d0b6e7c3e18f4e4
Vladimir <v.a.kuznetsov-83@yandex.ru>
2024-03-10 21:08:48 +0300

    Test commit

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ sed -i -- "s/%SERVER%/$1/g" /etc/nginx/conf.d/demo.conf
 service nginx restart
 composer install -q
 service php8.1-fpm restart
-sed -i -- "s/%DATABASE_HOST%/$2/g" /var/www/demo.env
-sed -i -- "s/%DATABASE_USER%/$3/g" /var/www/demo.env
-sed -i -- "s/%DATABASE_PASSWORD%/$4/g" /var/www/demo.env
-sed -i -- "s/%DATABASE_NAME%/$5/g" /var/www/demo.env
+sed -i -- "s/%DATABASE_HOST%/$2/g" /var/www/demo/.env
+sed -i -- "s/%DATABASE_USER%/$3/g" /var/www/demo/.env
+sed -i -- "s/%DATABASE_PASSWORD%/$4/g" /var/www/demo/.env
+sed -i -- "s/%DATABASE_NAME%/$5/g" /var/www/demo/.env
 php bin/console doctrine:migrations:migrate --no-interaction
-sed -i -- "s/%RABBITMQ_HOST%/$6/g" /var/www/demo.env
-sed -i -- "s/%RABBITMQ_USER%/$7/g" /var/www/demo.env
-sed -i -- "s/%RABBITMQ_PASSWORD%/$8/g" /var/www/demo.env
+sed -i -- "s/%RABBITMQ_HOST%/$6/g" /var/www/demo/.env
+sed -i -- "s/%RABBITMQ_USER%/$7/g" /var/www/demo/.env
+sed -i -- "s/%RABBITMQ_PASSWORD%/$8/g" /var/www/demo/.env
