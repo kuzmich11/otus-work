972989d3b23795147639634312c67da5d2ca3ff5
vladimir <v.a.kuznetsov-83@yandex.ru>
2024-03-11 20:32:40 +0300

    Test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cp deploy/nginx.conf /etc/nginx/conf.d/demo.conf -f
 sed -i -- "s|%SERVER_NAME%|$1|g" /etc/nginx/conf.d/demo.conf
 service nginx restart
-composer install -q
+composer install
 service php8.1-fpm restart
 sed -i -- "s|%DATABASE_HOST%|$2|g" /var/www/demo/.env
 sed -i -- "s|%DATABASE_USER%|$3|g" /var/www/demo/.env
