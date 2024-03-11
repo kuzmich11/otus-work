320aeb9de0b28614afaf16831cae73cb696f68f1
Vladimir <v.a.kuznetsov-83@yandex.ru>
2024-03-11 23:15:50 +0300

    Test commit

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
+composer install
 cp deploy/nginx.conf /etc/nginx/conf.d/demo.conf -f
 sed -i -- "s|%SERVER_NAME%|$1|g" /etc/nginx/conf.d/demo.conf
 service nginx restart
-composer install
 service php8.1-fpm restart
 sed -i -- "s|%DATABASE_HOST%|$2|g" /var/www/demo/.env
 sed -i -- "s|%DATABASE_USER%|$3|g" /var/www/demo/.env
